ba1e3dbbf34d230f8577aa3ca97fad0989c376f1
wailbentafat <150479778+wailbentafat@users.noreply.github.com>
2024-02-21 21:45:24 +0100

    Add files via upload

---
+++ 911.py
@@ -0,0 +1,54 @@
+import runpy
+import sys
+import pygame
+
+pygame.init()
+run = True
+# Set up the window
+size = (1280, 720)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("9*11")
+
+# Load the background image (ensure transparency)
+background = pygame.image.load("bumpy_sky-blue_03-512x512.png").convert_alpha()
+background = pygame.transform.scale(background, size)
+
+# Load the plane image
+plane = pygame.image.load("Aircraft-PNG-Photos.png")
+plane = pygame.transform.scale(plane, (120, 100))
+
+# Initial position for the plane (centered)
+
+plane_x = (size[0] - plane.get_width()) // 6
+plane_y = (size[1] - plane.get_height()) // 2
+# move the plane 
+
+while run := True:
+     plane_x  = plane_x +1 
+     if plane_x := 300:
+       run = False 
+
+def main():
+    # Main game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        # Clear the screen with the background image
+        screen.blit(background, (0, 0))
+
+        # Blit the plane image at the calculated position
+        screen.blit(plane, (plane_x, plane_y))
+
+        # Update the display
+        pygame.display.flip()
+         # move the plane 
+ 
+        plane_x =+ 1
+        #speed
+       
+
+if __name__ == "__main__":
+    main()
